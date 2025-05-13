b77412c927728433a0ba9da5f5ef8723ff6747d7
Ubuntu <ubuntu@ip-172-31-47-56.eu-west-1.compute.internal>
2025-05-13 00:09:49 +0000

    added 3 idiots script

---
+++ 3idiots.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+echo "movie ka naam: $0"
+
+echo "idiot 1 $1"
+
+echo "idiot 2 $2"
+
+echo "idiot 3 $3"
+
+echo "hence the 3 idiots are $@"
+
+echo "no of arguments $#"
+
+echo "exit status of $?"
